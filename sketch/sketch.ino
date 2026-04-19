c1d45fc7615995b4479e5e5194c5473df7afd673
kany-e <64484935+kany-e@users.noreply.github.com>
2026-04-18 22:51:57 -0700

    Refactor sketch.ino to simplify setup and loop

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -6,13 +6,11 @@
 
 // ---- Global hardware objects (defined once here) ----
 ModulinoThermo    thermo;
-ModulinoMovement  movement;
 Arduino_LED_Matrix matrix;
 
 // ---- Shared state (set by sensor functions, read by bridge API) ----
 int  lastDistanceMm   = 9999;
 bool presenceDetected = false;
-int  lastMovementEvent = 0;
 
 // ---- Forward declarations (functions defined in other .ino files) ----
 void initDisplay();
@@ -21,14 +19,6 @@ void scrollMessage(const char* msg, int frameDelay);
 void initSensors();
 int  getLightLevel();
 void updateDistance();
-
-void initMovement();
-void updateMovement();
-void captureMovementBaseline();
-bool isBaselineReady();
-
-void initLCD();
-void updateLCD(float tempC, float humidity, int light, bool presence, int eventCode);
 
 void registerBridgeAPI();
 
@@ -44,38 +34,26 @@ void setup() {
 
   Modulino.begin(Wire1);
   thermo.begin();
-  movement.begin();
 
   initDisplay();
   initSensors();
-  initMovement();
-  initLCD();
   registerBridgeAPI();
 }
 
 void loop() {
   updateDistance();
-  updateMovement();
 
   if (millis() - lastDisplayUpdate >= 500) {
     lastDisplayUpdate = millis();
 
-    // Read current sensor values (reused for both displays)
-    float curTemp  = thermo.getTemperature();
-    float curHum   = thermo.getHumidity();
-    int   curLight = getLightLevel();
-
-    // Update LCD with plant messages
-    updateLCD(curTemp, curHum, curLight, presenceDetected, lastMovementEvent);
-
-    // Update LED matrix (rotating temp/humidity and light)
     char msg[32];
     if (displayMode == 0) {
-      int t = (int)curTemp;
-      int h = (int)curHum;
+      int t = (int)thermo.getTemperature();
+      int h = (int)thermo.getHumidity();
       snprintf(msg, sizeof(msg), " %dC %d%% ", t, h);
     } else {
-      snprintf(msg, sizeof(msg), " L %d%% ", curLight);
+      int l = getLightLevel();
+      snprintf(msg, sizeof(msg), " L %d%% ", l);
     }
 
     scrollMessage(msg, 80);
