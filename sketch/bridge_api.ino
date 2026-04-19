23e8469dff5e3ecce9d9537b0e790b2712869240
kany-e <64484935+kany-e@users.noreply.github.com>
2026-04-18 22:51:06 -0700

    Remove movement and baseline functions from API
    Removed unused functions from Bridge API registration.

--- sketch/bridge_api.ino
+++ sketch/bridge_api.ino
@@ -4,9 +4,7 @@ float getHumidity();
 int   getLightLevel();
 int   getDistanceMm();
 bool  getPresence();
-int   getMovementEvent();
-void  recalibrateBaseline();
-bool  isBaselineReady();
+
 
 void registerBridgeAPI() {
   Bridge.provide("getTempC",             getTempC);
@@ -14,7 +12,4 @@ void registerBridgeAPI() {
   Bridge.provide("getLightLevel",        getLightLevel);
   Bridge.provide("getDistanceMm",        getDistanceMm);
   Bridge.provide("getPresence",          getPresence);
-  Bridge.provide("getMovementEvent",     getMovementEvent);
-  Bridge.provide("recalibrateBaseline",  recalibrateBaseline);
-  Bridge.provide("isBaselineReady",      isBaselineReady);
 }
